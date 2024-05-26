a350d7907a81e156238f3e4e755c3612357c0e78
rumourscape <advaitjoglekar@yahoo.in>
2024-05-26 21:29:25 +0530

    Added system instructions and improved performance

--- gradio-ui.py
+++ gradio-ui.py
@@ -4,16 +4,22 @@ import google.generativeai as genai
 from stocks_agent import get_stock
 
 genai.configure(api_key='AIzaSyDap6T0UIajYoMUXLjlcb7yf0_4EDD27Xs')
-model = genai.GenerativeModel( model_name='gemini-1.5-flash',
-                               tools=[get_stock] )
+model = genai.GenerativeModel(model_name='gemini-1.5-flash',
+                              system_instruction='''You are a helpful financial assistant. Your name is Stock Bro.
+                                                    You were created by Advait and Hasan for the benefit of the user. Feel free to use emojis.
+                                                    You are an agent that provides useful information and summaries about stocks.
+                                                    Your goal is to help the user navigate the stock markets and give sound financial help.
+                                                    You have access to tools that provide stock information. Include links from context in your answers.
+                                                    Prefer using relevent context from your history before calling a tool.
+                                                    Avoid asking the user follow up questions unless you really need to.    
+                                                    Answer all their queries promptly to the best of your ability, using the tools and context provided.
+                                                ''',
+                              tools=[get_stock]
+                              )
 
 chat = model.start_chat(enable_automatic_function_calling=True)
 
-def random_response(message, history):
-    print(history)
-    return random.choice(["Yes", "No"])
-
-def stock_agent(query, history):
+def stock_agent(query, history):    
     if history==[]:
         chat.history = []
     
@@ -25,7 +31,12 @@ gr.ChatInterface(
     title="Stock Bro",
     description="Your personal stock assistant!",
     textbox=gr.Textbox(placeholder="Ask me anything about stocks!", container=False, scale=7),
-    examples=["Is NVIDIA going up?", "What is the stock price of Apple?"],
+    examples=[
+        "Is NVIDIA going up?", 
+        "What is the stock price of Apple?", 
+        "Give me a summary on Zomato. What is the public opinion of this stock?",
+        "What is the latest information on Logitech?"
+        ],
     cache_examples=False,
     retry_btn=None,
     undo_btn=None,
